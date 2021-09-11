fc56e0c6bdb425f3bd947b2987374189da702959
victorhelopes <victorhelopes@gmail.com>
2021-09-11 16:22:08 -0300

    GRPC server communication

--- TrabParte1/src/client/Admin/admin.py
+++ TrabParte1/src/client/Admin/admin.py
@@ -1,43 +1,48 @@
-from server.User.PortalCliet import PortalClient
-from server.Admin.PortalAdmin import PortalAdmin
+import logging;
 
-class admin:
+from server.Admin.admin_pb2_grpc import PortalAdminStub;
+from server.Admin.admin_pb2 import Request;
+
+import grpc
+class admin():
     def getActivity():
-        print('What activity would you like to do?')
-        print('1 - get all activities')
-        print('2 - create an activity')
-        print('3 - update an activity')
-        print('4 - delete an activity')
-        print('5 - quit')
-        activity = int(input('What user would you like to do?'))
+        channel = grpc.insecure_channel('localhost:50051')
+        stub = PortalAdminStub(channel)
+        while not(activity == 5):
+            print('What activity would you like to do?')
+            print('1 - get all activities')
+            print('2 - create an activity')
+            print('3 - update an activity')
+            print('4 - delete an activity')
+            print('5 - quit')
+            activity = int(input('What user would you like to do?'))
 
-        if(activity == 1):
-            admin.getUser()
-        if(activity == 2):
-            admin.createUser()
-        if(activity == 3):
-            admin.updateuser()
-        if(activity == 4):
-            admin.deleteUser()
-        if(activity == 5):
-            print('bye')
-            return 1
-        if(activity > 5 or activity < 1):    
-            print('user does not exists')
+            if(activity == 1):
+                response = stub.getUser()
+                print('Id do usuario',response.message)
 
-    def createUser():
-        name = input('Qual o nome do novo usuario: ')
-        return PortalAdmin.createUser(name)
-    
-    def updateUser(name):
-        name = input('Qual o nome do novo usuario: ')
-        return(PortalAdmin.updateUser(name))
+            if(activity == 2):
+                PersonName = input("What's your name? ")
+                response = stub.createUser(Request(name=PersonName))
+                print('Id do usuario',response.message)
 
+            if(activity == 3):
+                PersonName = input("What's the name of the person would like to update? ")
+                response = stub.updateUser(Request(name=PersonName)) # i have to pass the new name of the person
+                print('Usuario atualizado',response.message)
 
-    def deleteUser():
-        name = input('Qual o nome do novo usuario: ')
-        return(PortalAdmin.updateUser(name))
+            if(activity == 4):
+                PersonName = input("What's the name of the person would like to update? ")
+                response = stub.deleteUser(Request(name=PersonName)) 
+                print('Usuario atualizado',response.message)
 
-    def getUser():
-        name = input('Qual o nome do novo usuario: ')
-        return(PortalAdmin.getUser(name)) 
+            if(activity == 5):
+                print('bye')
+                return 1
+
+            if(activity > 5 or activity < 1):    
+                print('user does not exists')
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    admin.getActivity()
--- TrabParte1/src/server/Admin/PortalAdmin.py
+++ TrabParte1/src/server/Admin/PortalAdmin.py
@@ -1,26 +1,47 @@
-from data.data import data 
+import sys
+sys.path.append("..\..\server")
 
-class PortalAdmin:
+from data import data 
+
+from concurrent import futures
+import logging
+
+import admin_pb2
+import admin_pb2_grpc
+
+import grpc
+
+class PortalAdmin(admin_pb2_grpc.PortalAdminServicer):
 
     def createUser(name):
         if(hash(name) > len(data.user)):
             for i in range(0,len(data.user) - hash(name)):
                 data.user.append(0)
         data.user[hash(name)] = {hash(name), name}
-        return (name)
+        return admin_pb2.Response(hash(name))
 
     def updateUser(name):
         if(data.user[hash(name)] == 0):
             return 'error'
         data.user[hash(name)] = {hash(name), name}
-        return 'OK'
+        return admin_pb2.Response('OK')
 
     def deleteUser(name):
         if(data.user[hash(name)] == 0):
             return 'error'
-        removed = data.user[hash(name)]
+        data.user[hash(name)]
         data.user[hash(name)] = 0
-        return removed
-
+        return admin_pb2.Response('removed')
     def getUser(name):
-        return (data.user[hash(name)])
+        return admin_pb2.Response(data.user[hash(name)])
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    admin_pb2_grpc.add_PortalAdminServicer_to_server(PortalAdmin(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    logging.baseConfig()
+    serve()
